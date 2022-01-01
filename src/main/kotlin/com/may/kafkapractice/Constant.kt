414901e5c04836330e56b831f114e85a6ad0035a
ayoung0073 <ayong0310@naver.com>
2022-01-02 02:55:17 +0900

    fix: 상수값 따로 관리

---
+++ src/main/kotlin/com/may/kafkapractice/Constant.kt
@@ -0,0 +1,9 @@
+package com.may.kafkapractice
+
+class Constant {
+    companion object {
+        const val TOPIC_NAME = "test_topic1"
+        const val GROUP_ID = "testgroup"
+        const val BOOTSTRAP_SERVERS = "127.0.0.1:9092"
+    }
+}
--- src/main/kotlin/com/may/kafkapractice/producer/SimpleProducer.kt
+++ src/main/kotlin/com/may/kafkapractice/producer/SimpleProducer.kt
@@ -1,5 +1,6 @@
 package com.may.kafkapractice.producer
 
+import com.may.kafkapractice.Constant
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.ProducerConfig
@@ -8,22 +9,18 @@ import org.apache.kafka.common.serialization.StringSerializer
 import java.lang.Exception
 import java.util.*
 
-private const val TOPIC_NAME = "test-topic"
-
-private const val BOOTSTRAP_SERVERS = "127.0.0.1:9092"
-
 fun main() {
     val configs = Properties()
-    configs[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = BOOTSTRAP_SERVERS
+    configs[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = Constant.BOOTSTRAP_SERVERS
     configs[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
     configs[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
     val producer: KafkaProducer<String, String> = KafkaProducer(configs)
     for (index in 0..9) {
         val data = "This is record $index"
-        val record = ProducerRecord<String, String>(TOPIC_NAME, data)
+        val record = ProducerRecord<String, String>(Constant.TOPIC_NAME, data)
         try {
             producer.send(record)
-            println("Send to $TOPIC_NAME | data : $data")
+            println("Send to $Constant.TOPIC_NAME | data : $data")
             Thread.sleep(1000)
         } catch (e: Exception) {
             println(e)
