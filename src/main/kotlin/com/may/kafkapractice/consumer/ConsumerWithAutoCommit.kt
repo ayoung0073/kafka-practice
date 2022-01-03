312e53d1ac3f85077a58fdf06d680d9e19a0967a
ayoung0073 <ayong0310@naver.com>
2022-01-03 23:44:47 +0900

    add: consumer auto commit으로 데이터가 중복 처리될 수 있음

--- src/main/kotlin/com/may/kafkapractice/consumer/ConsumerWithAutoCommit.kt
+++ src/main/kotlin/com/may/kafkapractice/consumer/ConsumerWithAutoCommit.kt
@@ -1,14 +1,20 @@
 package com.may.kafkapractice.consumer
 
 import com.may.kafkapractice.Constant
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 
 import java.time.Duration
 
 fun main() {
-    val consumer: KafkaConsumer<String, String> = KafkaConsumer(CustomConsumerConfig.get())
+    val configs = CustomConsumerConfig.get()
+    configs[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = true
+    configs[ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG] = 5000 // 5초마다 poll을 호출하여 자동으로 commit을 한다.
+
+    val consumer: KafkaConsumer<String, String> = KafkaConsumer(configs)
     consumer.subscribe(listOf(Constant.TOPIC_NAME))
+
     while (true) {
         val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(1))
         for (record in records) {
